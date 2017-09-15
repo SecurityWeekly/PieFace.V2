851fc9b3a078771e0310692d8ba00e7bdf1ea197
wpaquin <paquinwyatt@gmail.com>
2017-09-15 12:27:11 -0400

    Initial Git Push

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import Form
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, BooleanField
+
+class ImageSetForm(Form):
+   imageSet = SelectField('ImageSet', choices = [('psw', 'PSW'),('esw', 'ESW'), ('ssw', 'SSW'), ('hnn', 'HNN'), ('sg', 'SG'),('sgshorts', 'SGSHORTS'), ('sgnews', 'SGNEWS'), ('static', 'STATIC')])
+   select = SubmitField("Select Image Set")
+   activate = SubmitField("Activate Image Set")
+
+   resWidth = TextField("resWidth")
+   resHeight = TextField("resHeight")
+   pauseTime = TextField("Pause Time in Seconds")
+   isDefault = BooleanField("Default Image Set:")
+   imageType = RadioField("Type", choices = [('image', 'Image'), ('video', 'Video')])
