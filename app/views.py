7639c6c3f744f2efb1d73c00fb3b66d807d2cfd7
wpaquin <paquinwyatt@gmail.com>
2017-09-18 15:29:51 -0400

    Separated database operations and forms

--- app/views.py
+++ app/views.py
@@ -2,22 +2,20 @@ from flask import render_template, request, url_for, redirect, jsonify
 from app import app
 from app.forms import ImageSetForm
 from app import hdmi_controller
+from app import database_handler
 import subprocess
-
-
-
-
-
 
 # declare new hdmi_controller instance
 
 hdmi_controller = hdmi_controller.hdmi_controller()
+database_handler = database_handler.database_handler()
 
 app.secret_key = 's3cr3t'
 
 
 @app.route('/displayoutputstatus')
-
+def potato():
+    print("Displaying output status")
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -26,25 +24,16 @@ def index():
     version = '0.1'
     current_image = "NONE"
     selectedSet = "No Set Selected Yet"
-    #   form = ImageSetForm()
+    form = ImageSetForm()
     imageset_form = ImageSetForm()
 
     imgchange_status = hdmi_controller.display_output_status
 
-
-
-
     # Coll
     if request.method == 'POST':
 
-        selectedSet = "potato tomato"
+        selectedSet = database_handler.select_all()
 
-        print("Request Form Fields= " + str(request.form) + "\n")
-        print("Active = " + str(request.form['active']) + "\n")
-        print("Select = " + str(request.form['select']) + "\n")
-
-        print("activate button was pressed" + imageset_form.activate.data)
-        print("select button was pressed" + imageset_form.select.data)
         if imageset_form.validate():
             print("activate button was pressed : {value}".format(value=imageset_form.activate.data))
             print("select button was pressed : {value}".format(value=imageset_form.select.data))
@@ -60,9 +49,10 @@ def index():
         # this works: c = subprocess.call(['/home/pi/display.sh', current_image])
         c = subprocess.Popen(['/home/pi/display.sh', current_image], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Right after I ran the command" + current_image)
-        return render_template('index.html', title='PieFace', hdmichange_status=hdmi_controller.get_hdmi_status, imgchange_status=hdmi_controller.display_output_status())
+        return render_template('index.html', title='PieFace', hdmichange_status=hdmi_controller.get_hdmi_status,
+                               imgchange_status=hdmi_controller.display_output_status())
 
-    hdmichange_status = "out1: " + hdmi_controller.output1
+    # hdmichange_status = "out1: " + hdmi_controller.output1
     try:
         hdmichange_status = hdmi_controller.get_hdmi_status()
     except:
@@ -76,5 +66,7 @@ def index():
                                selected=selectedSet,
                                hdmichange_status=hdmi_controller.get_hdmi_status,
                                imgchange_status=hdmi_controller.display_output_status())
+
+
 
     print(hdmichange_status)
