5e6e4c2f6d5af4d022c23f88c30578daa6d922d7
wpaquin <paquinwyatt@gmail.com>
2017-09-18 16:28:02 -0400

    Image Player

--- app/client/vid_player.py
+++ app/client/vid_player.py
@@ -1,4 +1,41 @@
-from subprocess import Popen
+# from subprocess import Popen
 
-movie1 = '/home/kyle/Desktop/vids/vid.mp4'
-omxc = Popen(['omxplayer', '-b', movie1])
+# movie1 = '/home/kyle/Desktop/vids/vid.mp4'
+# omxc = Popen(['omxplayer', '-b', movie1])
+
+
+
+
+
+
+from tkinter import Tk, Canvas
+from PIL import ImageTk, Image
+
+
+# root = tkinter.Tk()
+#
+# photo = tkinter.PhotoImage(file="image.gif")
+# img1 = tkinter.Label(root, image = photo)
+# img1.pack()
+# root.mainloop()
+
+
+
+
+root = Tk()
+root.attributes("-fullscreen", True)
+
+# Create a canvas
+canvas = Canvas(root, width=9000, height=300)
+canvas.pack()
+
+# Load the image file
+im = Image.open('download.jpg')
+# Put the image into a canvas compatible class, and stick in an
+# arbitrary variable to the garbage collector doesn't destroy it
+canvas.image = ImageTk.PhotoImage(im)
+# Add the image to the canvas, and set the anchor to the top left / north west corner
+canvas.create_image(0, 0, image=canvas.image, anchor='nw')
+
+root.mainloop()
+
