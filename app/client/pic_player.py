26851b09d85679e4ad4182fd887db0c50eeefb56
wpaquin <paquinwyatt@gmail.com>
2017-09-19 12:11:59 -0400

    Cleanup

--- app/client/pic_player.py
+++ app/client/pic_player.py
@@ -40,9 +40,9 @@ delay = 3500
 # get a series of gif images you have in the working folder
 # or use full path, or set directory to where the images are
 image_files = [
-    Image.open('download.jpg').resize((1920, 1080), Image.ANTIALIAS),
-    Image.open('download2.png').resize((1920, 1080), Image.ANTIALIAS),
-    Image.open('download3.jpg').resize((1920, 1080), Image.ANTIALIAS)
+    Image.open('images/download.jpg').resize((App.winfo_screenheight(), App.winfo_screenwidth()), Image.ANTIALIAS),
+    Image.open('images/download2.png').resize((1920, 1080), Image.ANTIALIAS),
+    Image.open('images/download3.jpg').resize((1920, 1080), Image.ANTIALIAS)
 ]
 # upper left corner coordinates of app window
 x = 100
