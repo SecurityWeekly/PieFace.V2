63c318f5757792e79534ebb34a8589d2d4789ead
Kyle Fontaine <kyle@securityweekly.net>
2017-09-22 11:28:58 -0400

    Fixed View Error

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ import subprocess
 # declare new hdmi_controller instance
 
 hdmi_controller = hdmi_controller.hdmi_controller()
-database_handler = database_handler.database_handler()
+# database_handler = database_handler.database_handler()
 
 app.secret_key = 's3cr3t'
 
@@ -16,6 +16,7 @@ app.secret_key = 's3cr3t'
 @app.route('/displayoutputstatus')
 def potato():
     print("Displaying output status")
+    return True
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -27,7 +28,7 @@ def index():
     form = ImageSetForm()
     imageset_form = ImageSetForm()
 
-    imgchange_status = hdmi_controller.display_output_status
+    # imgchange_status = hdmi_controller.display_output_status
 
     # Coll
     if request.method == 'POST':
@@ -48,29 +49,32 @@ def index():
         elif imageset_form.select.data == True:
             selectedSet = request.form['imageSet']
 
-    if current_image != "NONE":
-        print("Changing the current image to: " + current_image)
-        # this works: c = subprocess.call(['/home/pi/display.sh', current_image])
-        c = subprocess.Popen(['/home/pi/display.sh', current_image], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("Right after I ran the command" + current_image)
-        return render_template('index.html', title='PieFace', hdmichange_status=hdmi_controller.get_hdmi_status,
-                               imgchange_status=hdmi_controller.display_output_status())
+            # if current_image != "NONE":
+            #     print("Changing the current image to: " + current_image)
+            #     # this works: c = subprocess.call(['/home/pi/display.sh', current_image])
+            #     c = subprocess.Popen(['/home/pi/display.sh', current_image], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            #     print("Right after I ran the command" + current_image)
+            #     return render_template('index.html', title='PieFace', hdmichange_status=hdmi_controller.get_hdmi_status,
+            #                            imgchange_status=hdmi_controller.display_output_status())
 
-    # hdmichange_status = "out1: " + hdmi_controller.output1
-    try:
-        hdmichange_status = hdmi_controller.get_hdmi_status()
-    except:
-        print("Error getting hdmi status!")
+            # hdmichange_status = "out1: " + hdmi_controller.output1
+            # try:
+            #     hdmichange_status = hdmi_controller.get_hdmi_status()
+            # except:
+            #     print("Error getting hdmi status!")
 
-        return render_template('index.html',
-                               imageset_form=imageset_form,
-                               title='PieFace',
-                               active=activeSet.upper(),
-                               version=1.0,
-                               selected=selectedSet,
-                               hdmichange_status=hdmi_controller.get_hdmi_status,
-                               imgchange_status=hdmi_controller.display_output_status())
+    return render_template('index.html',
+                           imageset_form=imageset_form,
+                           title='PieFace',
+                           active=activeSet.upper(),
+                           version=1.0,
+                           selected=selectedSet,
+                           hdmichange_status="hdmi_controller.get_hdmi_status",
+                           imgchange_status="hdmi_controller.display_output_status()")
 
 
 
-    print(hdmichange_status)
+
+
+
+    # print(hdmichange_status)
