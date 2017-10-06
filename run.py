36cbfcf0838dff54e6c0e0318852ac9224d70c4b
Wyatt Paquin <paquinwyatt@gmail.com>
2017-10-06 13:40:35 -0400

    diff platforms

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 #!venv/bin/python
 from app import app
+import platform
 
-app.run('localhost', 5001, debug=True)
+print("You are running " + platform.system())
+if platform.system() == 'Linux':
+    app.run('localhost', 5001, debug=True)
+else:
+    app.run('0.0.0.0', 5001, debug=True)
